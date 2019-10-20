0d704e1b46d225c44389575f4b3eac543a98bbbc
Vadim Perevoz <enocryph@yandex.ru>
2019-10-20 15:22:53 +0300

    fixed watch task for windows

--- gulp/src/tasks/watch.js
+++ gulp/src/tasks/watch.js
@@ -6,6 +6,7 @@ module.exports = () => {
         `${config.sourcePath}/${config.stylesDirectory}/**/*.sass`,
         `${config.sourcePath}/${config.stylesDirectory}/**/*.css`
       ],
+      {usePolling: true},
       gulp.series('styles'));
     gulp.watch(
       [
@@ -13,18 +14,20 @@ module.exports = () => {
         `${config.sourcePath}/${config.viewsDirectory}/**/*.html`,
         `${config.sourcePath}/${config.viewsDirectory}/**/*.htm`
       ],
+      {usePolling: true},
       gulp.series('templates'));
     gulp.watch(
       [
         `./${config.sourcePath}/${config.javascriptDirectory}/**/*.js`,
         `!./${config.sourcePath}/${config.javascriptDirectory}/libraries.js`
       ],
+      {usePolling: true},
       gulp.series('scripts'));
-    gulp.watch([`${config.sourcePath}/${config.pngSpriteDirectory}/*.png`], gulp.series('png-sprite'));
-    gulp.watch([`./${config.sourcePath}/${config.imagesDirectory}/**/*`], gulp.series('images:copy'));
-    gulp.watch([`./${config.sourcePath}/${config.fontsDirectory}/**/*`], gulp.series('fonts:copy'));
-    gulp.watch([`./${config.sourcePath}/${config.svgSpriteDirectory}/**/*.svg`], gulp.series('svg:sprite'));
-    gulp.watch([`./${config.sourcePath}/${config.svgInlineSpriteDirectory}/**/*.svg`], gulp.series('svg:inline'));
-    gulp.watch([`./${config.sourcePath}/${config.iconicFontDirectory}/**/*.svg`], gulp.series('iconic'));
+    gulp.watch([`${config.sourcePath}/${config.pngSpriteDirectory}/*.png`], {usePolling: true}, gulp.series('png-sprite'));
+    gulp.watch([`./${config.sourcePath}/${config.imagesDirectory}/**/*`], {usePolling: true}, gulp.series('images:copy'));
+    gulp.watch([`./${config.sourcePath}/${config.fontsDirectory}/**/*`], {usePolling: true}, gulp.series('fonts:copy'));
+    gulp.watch([`./${config.sourcePath}/${config.svgSpriteDirectory}/**/*.svg`], {usePolling: true}, gulp.series('svg:sprite'));
+    gulp.watch([`./${config.sourcePath}/${config.svgInlineSpriteDirectory}/**/*.svg`], {usePolling: true}, gulp.series('svg:inline'));
+    gulp.watch([`./${config.sourcePath}/${config.iconicFontDirectory}/**/*.svg`], {usePolling: true}, gulp.series('iconic'));
   });
 };
